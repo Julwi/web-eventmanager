83a0a321b322e5df925a6693564d9fb321cee4c2
Julian Wingenfeld <Julian.Wingenfeld@gmx.de>
2020-10-24 13:43:41 +0200

    created table for archive.html

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User (db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    # Additional query in the background that connects user with events
+    # Additional backround query that connects user with events
     events = db.relationship('Event', backref="author", lazy=True)
 
     def __repr__(self):
